2fa8024638b14259fe4f1cf9c5601639ed02a4fe
ilayRavia <ilayravia@gmail.com>
2020-03-17 16:03:51 +0200

    add heb month enum

---
+++ HebMonthEnum.js
@@ -0,0 +1,5 @@
+const Enum = require('enum');
+
+var HebMonthEnum = new Enum(['Nisan', 'Iyyar', 'Sivan', 'Tamuz', 'Av', 'Elul', 'Tishrei', 'Cheshvan', 'Kislev', 'Tevet', 'Shvat', 'Adar'],'HebMonth');
+
+module.exports= HebMonthEnum;
