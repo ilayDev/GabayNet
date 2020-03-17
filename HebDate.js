92b21bbd6d58b6cbebd7708d051ec0b43a142c83
ilayRavia <ilayravia@gmail.com>
2020-03-17 16:09:52 +0200

    add heb date class

---
+++ HebDate.js
@@ -0,0 +1,17 @@
+const HebMonthEnum = require('./HebMonthEnum');
+
+class HebDate{
+    day;
+    month;
+    year;
+
+
+    constructor(d,m,y){
+        this.day = d;
+        this.month = m;
+        this.year = y;
+    }
+
+
+}
+module.exports= HebDate;
